56874dc860859c087ca4f00086507330b01843f2
Sahil Hinge <sahilhinge89@gmail.com>
2026-09-03 16:13:45 +0530

    feat: add OTP model

---
+++ server/models/OTP.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+   email:{
+    type:String,
+    required:true,
+   },
+   otp:{
+    type:String,
+    required:true,
+   },
+   createdAt:{
+    type:Date,
+    default:Date.now(),
+    expires:5*60
+   }
+
+}) 
+
+// creating function to send emails
+
+async function sendVerificationEmail(email,otp){
+ try {
+   const mailResponse =await mailSender (email,"Verification Email from StudyKaro",otp);
+   
+ } catch (error) {
+   console.log("error occured while sending mail:",error);
+   throw error;
+ }
+}
+
+// applying pre save middleware
+otpSchema.pre("save",async function (next){
+   await sendVerificationEmail(this.email, this.otp);
+   next();
+})
+
+module.exports = mongoose.model("OTP",otpSchema);
