796c11331de634231a51c826ee43551f5924a00e
Sahil Hinge <sahilhinge89@gmail.com>
2026-09-09 13:48:51 +0530

    Add array in Course object

--- server/models/Tags.js
+++ server/models/Tags.js
@@ -8,10 +8,10 @@ const tagsSchema = new mongoose.Schema({
    description:{
     type:String,
    },
-   Course:{
+   Course:[{
     type:mongoose.Schema.Types.ObjectId,
     ref:"Courses"
-   }
+   }] 
 
 }) 
 
