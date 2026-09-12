0de226c8a02ef708f33cdd54f436960bd3a9561d
Sahil Hinge <sahilhinge89@gmail.com>
2026-09-12 11:02:56 +0530

    add packages

--- server/models/Courses.js
+++ server/models/Courses.js
@@ -37,7 +37,7 @@ const coursesSchema  = new mongoose.Schema({
          
          ref:"Tags"
     },
-    StudentsEnrolled :[{
+    studentsEnrolled :[{
          type:mongoose.Schema.Types.ObjectId,
          required:true,
          ref:"User"
